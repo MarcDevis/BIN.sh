be58e21c0428ec481b3c67946e10a5e240aa5015
Your Name <you@example.com>
2024-05-31 09:12:34 +0200

    aleatporia

--- bot_telegram/mensaje_telegram.sh
+++ bot_telegram/mensaje_telegram.sh
@@ -7,8 +7,8 @@ then
     exit 1
 fi
 MISSATGE=${1}
-TOKEN="POSA-AQUI-EL-TEU-TOKEN"
-ID="POSA-AQUI-LA-TEVA-ID"
+TOKEN="6568313886:AAGu1SO6wPXv81TJtktm5SE32FByQQdPVgo"
+ID="6183616286"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 curl -s -X POST ${URL} -d chat_id=${ID} -d text="${MISSATGE}"
